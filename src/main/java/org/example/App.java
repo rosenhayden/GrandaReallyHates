723c620b7a7406da9c861d93fd2421221b90edf3
rosen.hayden <rosen.hayden@vashonsd.org>
2022-01-28 09:44:22 -0800

    LOL

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,13 +4,13 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 /**
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     static List<String> knownWords;
 
     public static void main(String[] args) {
@@ -19,23 +19,55 @@ public class App
         try {
             BufferedReader br = new BufferedReader(new FileReader("common_words.txt"));
             String nextWord;
-            while((nextWord = br.readLine()) != null) {
+            while ((nextWord = br.readLine()) != null) {
                 knownWords.add(nextWord);
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println("Hello, I'm Grandma Betty");
-
-
-    }
-
-    /**
-     * This method should return true if the given String has consecutive letters that are the same.
-     * @param str -- the String in question.
-     * @return true if str has any consecutive letters that are identical; false if not.
-     */
-    public static boolean hasDoubleLetters(String str) {
-        return false;
+        int i = 5;
+        while (i > 0) {
+            Grandma grandma = new Jim();
+            System.out.println(grandma.getName());
+            System.out.println("Enter a word here:");
+            Scanner scan = new Scanner(System.in);
+            String str = scan.nextLine();
+            if (grandma.likesWord(str)) {
+                System.out.println("I like that word!");
+            }
+            else if(!knownWords.contains(str)) {
+                System.out.println("I'm not sure what that word means.");
+            }
+            else {
+                System.out.println("That word is trash!");
+                --i;
+                System.out.println("You have " + i + " trys left...");
+            }
+        }
     }
 }
+
+//        int i = 5;
+//        while (i > 0) {
+//
+//            Scanner scan = new Scanner(System.in);
+//            String word = scan.nextLine();
+//            hasDoubleLetters(word);
+//             if(hasDoubleLetters(word) == false) {
+//                System.out.println("I hate that word");
+//                --i;
+//                System.out.println("You have " + i + " trys left!");
+//
+//            } else {
+//                System.out.println("I love that word");
+//                System.out.println("You have " + i + " trys left!");
+//
+//            }
+//        }
+//    }
+
+
+
+
+
+
